040bbb0b76a024f5c32c2adde324bbece7f2150b
hamza-133 <hamzabouelwahab04@gmail.com>
2026-01-07 22:51:44 +0100

    feat: enable updating existing tasks

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,10 @@ let divOfUl = document.querySelector(".divOf-list")
 const taskInput = document.querySelector("#task-input")
 const taskBtn = document.querySelector("#task-btn")
 let errors 
+let currentEditingTask = null
+
 taskInput.addEventListener("change" ,() => {
-    inputResult = taskInput.value
+    inputResult = taskInput.value.trim()
 
 })
 taskInput.addEventListener("input" , ()=> {
@@ -16,19 +18,64 @@ taskInput.addEventListener("input" , ()=> {
 
 taskBtn.addEventListener("click" , () => {
     if (!inputResult) {
+        if (!errors) {
         errors = document.createElement("div")
         errors.setAttribute("id" , "error-ofinput")
         errors.textContent = "write some think"
         errors.style.color = "red"
         divOfUl.appendChild(errors)
-        
-    }else{
-        const listOfUsers = document.createElement("li")
-        listOfUsers.setAttribute("class" , "list-of-users")
-        listOfUsers.textContent = inputResult 
-        pushList.appendChild(listOfUsers)
+        }
+        return
+    }
+
+    if (currentEditingTask) {
+        currentEditingTask.textContent = inputResult
         taskInput.value = ""
         inputResult = null
+        currentEditingTask = null
+        taskBtn.textContent = "Add"
+    }
+    else{
+        const divOfList = document.createElement("div")
+
+        const listOfUsers = document.createElement("li")
+        divOfList.setAttribute("class" , "list-of-users")
+        
+        listOfUsers.textContent = inputResult 
+        
+        const divIcons = document.createElement("div")
+        divIcons.setAttribute("class" , "divIcons")
+
+        const update = document.createElement("span")
+        update.innerHTML = `<i class="bi bi-pencil"></i>`
+
+        const verrif = document.createElement("span")
+        verrif.innerHTML = `<i class="bi bi-check-lg"></i>`
+
+        const deletLi = document.createElement("span")
+        deletLi.innerHTML = `<i class="bi bi-trash3"></i>`
+
+        pushList.appendChild(divOfList)
+        divOfList.appendChild(listOfUsers)
+        divOfList.appendChild(divIcons)
+        divIcons.appendChild(update)
+        divIcons.appendChild(verrif)
+        divIcons.appendChild(deletLi)
+        
+        taskInput.value = ""
+        inputResult = null
+
+        update.addEventListener("click" ,()=>{
+            taskInput.value = listOfUsers.textContent
+            inputResult = listOfUsers.textContent
+            currentEditingTask = listOfUsers
+            taskBtn.textContent = "Update"
+            taskInput.focus()
+        })
+
+
+        
     }
 })
 // * githuuuuuuuuuuuuuuuuuuuuuuuub .
+
